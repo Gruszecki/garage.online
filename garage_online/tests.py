e0a691089f9edd529c215d117c5d03f187c3b7c1
Gruszecki <wojtek.gruszecki@gmail.com>
2022-06-26 15:30:29 +0200

    Fix: unique email adresses in database

--- garage_online/tests.py
+++ garage_online/tests.py
@@ -1,3 +1,42 @@
+import pytest
 from django.test import TestCase
+from garage_online.forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_user_register_form_valid():
+    # Arrange
+    form = CustomUserCreationForm({
+        'username': 'WojciechGruszecki',
+        'email': 'wojciech.gruszecki@gmail.com',
+        'password1': 'Trudne#Hasło#692137420',
+        'password2': 'Trudne#Hasło#692137420'
+    })
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result
+
+
+@pytest.mark.django_db
+def test_user_register_form_invalid_email_already_in_db():
+    # Arrange
+    User.objects.create(email='wojciech.gruszecki@gmail.com')
+
+    form = CustomUserCreationForm({
+        'username': 'WojciechGruszecki',
+        'email': 'wojciech.gruszecki@gmail.com',
+        'password1': 'Trudne#Hasło#692137420',
+        'password2': 'Trudne#Hasło#692137420'
+    })
+
+    # Action
+    result = form.is_valid()
+
+    # Assert
+    assert result is False
+    assert form.errors['email'][0] == 'Error email'
--- garage_online/views.py
+++ garage_online/views.py
@@ -50,25 +50,31 @@ def register(request):
         )
     elif request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
+
         if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
+            user_email = form.cleaned_data.get('email')
+            if not User.objects.filter(email=user_email):
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
 
-            current_site = get_current_site(request)
-            mail_subject = 'Aktywacja konta w Garażu'
-            mail_message = render_to_string('garage_online/register_confirm.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, mail_message, to=[to_email])
-            email.send()
+                current_site = get_current_site(request)
+                mail_subject = 'Aktywacja konta w Garażu'
+                mail_message = render_to_string('garage_online/register_confirm.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(mail_subject, mail_message, to=[to_email])
+                email.send()
 
-            messages.success(request, 'Potwierdź, proszę, swój e-mail w celu dokończenia rejestracji.')
-            return redirect('all_bands')
+                messages.success(request, 'Potwierdź, proszę, swój e-mail w celu dokończenia rejestracji.')
+                return redirect('all_bands')
+            else:
+                messages.success(request, "Podany adres e-mail znajduje się już w bazie.")
+                return redirect('all_bands')
 
         messages.success(request, "Coś poszło nie tak :\\")
         return redirect(all_bands)
