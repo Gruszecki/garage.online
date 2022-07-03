cfbf085503fc48757f0be8a4b1e83c3761429d8e
Gruszecki <wojtek.gruszecki@gmail.com>
2022-07-03 18:11:50 +0200

    Feature: color change

--- garage_project/settings.py
+++ garage_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['https://garageonline.herokuapp.com/', 'garageonline.herokuapp.com', 'localhost:8000', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['https://garageonline.herokuapp.com/', 'garageonline.herokuapp.com', 'localhost:8000', '127.0.0.1:8000']
 
 
 # Application definition
