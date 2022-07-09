1486ae0918a31bf7b062f8917d1315eb88d06a07
Gruszecki <wojtek.gruszecki@gmail.com>
2022-07-09 18:05:37 +0200

    Code refactoring part 2

--- garage_project/settings.py
+++ garage_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['https://garageonline.herokuapp.com/', 'garageonline.herokuapp.com', 'garageonline.pl']
+ALLOWED_HOSTS = ['https://garageonline.herokuapp.com/', 'garageonline.herokuapp.com', 'garageonline.pl']
 
 
 # Application definition
